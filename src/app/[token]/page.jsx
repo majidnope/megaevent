d096bf6dd46d904fed5328c12b6f7bb217537f66
majid-nope <muhammed0000majid@gmail.com>
2024-06-22 18:11:42 +0530

    warning

--- src/app/[token]/page.jsx
+++ src/app/[token]/page.jsx
@@ -20,6 +20,7 @@ const Page = () => {
 
     function captureScreenshot() {
         // Ensure styles are fully applied by forcing reflow
+        setLoading1(true)
         const element = ToCaptureRef.current;
         if (element) {
             element.style.display = 'none';
@@ -28,28 +29,34 @@ const Page = () => {
         }
 
         // Add a delay to ensure rendering is complete
-        setTimeout(() => {
-            html2canvas(ToCaptureRef.current, { useCORS: true })
-                .then((canvas) => {
-                    console.log(canvas);
-                    var dataURL = canvas.toDataURL("image/png");
-                    var img = new Image();
-                    img.src = dataURL;
-                    img.download = dataURL;
-                    var a = document.createElement("a");
-                    a.innerHTML = "DOWNLOAD";
-                    a.target = "_blank";
-                    a.href = img.src;
-                    a.download = img.download;
-                    a.style.display = "none";
-                    document.body.appendChild(a);
-                    a.click();
-                });
-        }, 100); // Adjust delay as needed
+
+        html2canvas(ToCaptureRef.current, { useCORS: true })
+            .then((canvas) => {
+                console.log(canvas);
+                var dataURL = canvas.toDataURL("image/png");
+                var img = new Image();
+                img.src = dataURL;
+                img.download = dataURL;
+                var a = document.createElement("a");
+                a.innerHTML = "DOWNLOAD";
+                a.target = "_blank";
+                a.href = img.src;
+                a.download = 'ticket.png';
+                document.body.appendChild(a);
+                a.click();
+                document.body.removeChild(a);
+                a.style.display = "none";
+
+
+                setLoading1(false)
+
+            });
+
     }
 
     const { token } = useParams();
     const [loading, setLoading] = React.useState(true);
+    const [loading1, setLoading1] = React.useState(false);
     const [payment, setPayment] = React.useState({});
 
     React.useEffect(() => {
@@ -81,7 +88,10 @@ const Page = () => {
             }} /> :
                 <>
                     <Ticket ref={ToCaptureRef} payment={payment} />
-                    <Button onClick={captureScreenshot}>Download Ticket</Button>
+                    <div>
+                        <span style={{ color: "red" }}>Musk keep this ticket</span>
+                        <Button loading={loading1} onClick={captureScreenshot}>Download Ticket</Button>
+                    </div>
                 </>
             }
         </div>
